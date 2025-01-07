1c530b111bd2e633103b5671fb04dfef4d476340
Sthefanie <sthefaniefernanda07@gmail.com>
2025-01-07 16:03:09 -0300

    implemento de get, post e put

--- index.js
+++ index.js
@@ -2,18 +2,40 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
 
+
+//listagem inicial de bandas
 const bandas = ['System of a down', 'Limp bizkit', 'Korn', 'Gojira']
 
-server.get('/bandas', (res,rest) =>{
-    return rest.json(cursos);
+server.get('/bandas', (req,res) =>{
+    return res.json(bandas);
 });
 
 //localhost:3000/api
-server.get('/api/:index', (req, rest)=>{
+server.get('/bandas/:index', (req, res)=>{
     const { index } = req.params;
 
-return rest.json(bandas[index]);
-})
+return res.json(bandas[index]);
+});
+
+//adicionando novas bandas
+server.post('/bandas', (req, res) =>{
+    const { nome } = req.body;
+    bandas.push(nome);
+
+    return res.json(bandas);
+});
+
+//atualizando as bandas
+server.put('/bandas/:index', (req, res)=>{
+    const { index } = req.params;
+    const { nome } = req.body;
+
+    bandas[index] = nome;
+
+    return res.json(bandas);
+});
 
 server.listen(3000);
+;
